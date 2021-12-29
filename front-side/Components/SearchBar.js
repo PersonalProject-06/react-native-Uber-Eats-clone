8f6c9a56a046e64694f36695dd2d62284475c2dd
firas-debich <debichf02@gmail.com>
2021-12-29 23:42:26 +0100

    adding icons

--- front-side/Components/SearchBar.js
+++ front-side/Components/SearchBar.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
-
+import React from "react";
+import { View, Text } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { Ionicons } from '@expo/vector-icons';
 export default function SearchBar() {
-    return (
-        <View>
-           <GooglePlacesAutocomplete placeholder='Search'/>
-        </View>
-    )
+  return (
+    <View style={{ marginTop: 15, flexDirection: "row" }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+            textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        
+        renderLeftButton={() =>(
+          <View style={{marginLeft:10}}>
+           <Ionicons name="md-location-sharp" size={24} color="black" />
+          </View>
+        )}
+        />
+      
+    </View>
+  );
 }
