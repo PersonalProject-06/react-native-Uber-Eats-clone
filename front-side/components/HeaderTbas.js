1a728a8cd2597a8fb3def0a3b2839bc4ec7baced
firas-debich <debichf02@gmail.com>
2021-12-29 21:34:23 +0100

    adding home secreen and header tabs component

---
+++ front-side/components/HeaderTbas.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function HeaderTbas() {
+    return (
+        <View>
+            <Text>Header tabs</Text>
+        </View>
+    )
+}
