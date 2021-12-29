fac2d807f31ffcf31363e600d570e2f6f665da7d
firas-debich <debichf02@gmail.com>
2021-12-30 00:06:08 +0100

    adding new image to assest file

--- front-side/Components/SearchBar.js
+++ front-side/Components/SearchBar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 export default function SearchBar() {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
       <GooglePlacesAutocomplete
         placeholder="Search"
         styles={{
-            textInput: {
+          textInput: {
             backgroundColor: "#eee",
             borderRadius: 20,
             fontWeight: "700",
@@ -22,30 +22,31 @@ export default function SearchBar() {
             marginRight: 10,
           },
         }}
-        
-        renderLeftButton={() =>(
-          <View style={{marginLeft:10}}>
-           <Ionicons name="md-location-sharp" size={24} color="black" />
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="md-location-sharp" size={24} color="black" />
           </View>
         )}
-
-        renderRightButton={()=>(
-            <View style={{
-                flexDirection:"row",
-                marginRight:8,
-                backgroundColor:"white",
-                padding:9,
-                borderRadius:30,
-                alignItems:"center",
-            }}>
-
-                
-                <AntDesign  name="clockcircle" size={11} style={{marginRight:6}}/>
-                <Text>Search</Text>
-            </View>
+        renderRightButton={() => (
+          <View
+            style={{
+              flexDirection: "row",
+              marginRight: 8,
+              backgroundColor: "white",
+              padding: 9,
+              borderRadius: 30,
+              alignItems: "center",
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={11}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
         )}
-        />
-      
+      />
     </View>
   );
 }
--- front-side/Screens/Home.js
+++ front-side/Screens/Home.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import {SafeAreaView,} from 'react-native-safe-area-context';
-import HeaderTabs from '../Components/HeaderTabs'
-import SearchBar from '../Components/SearchBar';
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Categories from "../Components/Categories";
+import HeaderTabs from "../Components/HeaderTabs";
+import SearchBar from "../Components/SearchBar";
 
 export default function Home() {
-    return (
-        <SafeAreaView style={{backgroundColor:"#eee",flex:1}}>
-            <View style={{backgroundColor:"white",padding:15}}>
-           <HeaderTabs/>
-             <SearchBar/>   
-            </View>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories/>
+    </SafeAreaView>
+  );
 }
-
-
