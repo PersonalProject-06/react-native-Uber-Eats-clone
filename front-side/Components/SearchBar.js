0989bf19069aa3c675766025ec0ef91b39d5e5bd
firas-debich <debichf02@gmail.com>
2021-12-29 23:57:57 +0100

    search bar component done

--- front-side/Components/SearchBar.js
+++ front-side/Components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { Ionicons } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 export default function SearchBar() {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
@@ -27,6 +27,22 @@ export default function SearchBar() {
           <View style={{marginLeft:10}}>
            <Ionicons name="md-location-sharp" size={24} color="black" />
           </View>
+        )}
+
+        renderRightButton={()=>(
+            <View style={{
+                flexDirection:"row",
+                marginRight:8,
+                backgroundColor:"white",
+                padding:9,
+                borderRadius:30,
+                alignItems:"center",
+            }}>
+
+                
+                <AntDesign  name="clockcircle" size={11} style={{marginRight:6}}/>
+                <Text>Search</Text>
+            </View>
         )}
         />
       
