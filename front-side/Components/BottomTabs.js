b03fddce54f01a9a43b74cf0d4b3fd2caa20e452
firas-debich <debichf02@gmail.com>
2021-12-30 11:56:31 +0100

    Bottom Tab done

--- front-side/Components/BottomTabs.js
+++ front-side/Components/BottomTabs.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { View, Text } from "react-native";
+const IconsName = [
+  {
+    icon: "home",
+    text: "Home",
+  },
+  {
+    icon: "search",
+    text: "Browse",
+  },
+  {
+    icon: "shopping-bag",
+    text: "Grocery",
+  },
+  {
+    icon: "receipt",
+    text: "Orders",
+  },
 
+  {
+    icon: "user",
+    text: "Account",
+  },
+];
 export default function BottomTabs() {
-    return (
-        <View style={{
-            flexDirection:"row",
-            margin:10,
-            justifyContent:"space-between",
-            marginHorizontal:30
-
-        }}>
-           
-            <Text>bottom</Text>
-            <Text>bottom</Text>
-            <Text>bottom</Text>
-        </View>
-    )
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        margin: 10,
+        justifyContent: "space-between",
+        marginHorizontal: 30,
+      }}
+    >
+      {IconsName.map((e, i) => (
+        <Icon icon={e.icon} text={e.text} />
+      ))}
+    </View>
+  );
 }
+
+const Icon = ({ icon, text }) => (
+  <View>
+    <FontAwesome5
+      name={icon}
+      size={24}
+      style={{
+        marginBottom: 3,
+        alignSelf: "center",
+      }}
+    />
+    <Text>{text}</Text>
+  </View>
+);
