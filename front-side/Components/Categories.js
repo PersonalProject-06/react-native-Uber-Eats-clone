8f2b61c49f4d0941a23a1bff89b66196b22c3f9c
firas-debich <debichf02@gmail.com>
2021-12-30 01:00:43 +0100

    categories compoenet done

--- front-side/Components/Categories.js
+++ front-side/Components/Categories.js
@@ -1,10 +1,68 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
+const items =[
+    {
+        image:require("../assets/images/shopping-bag.png"),
+        text:"Pick-up"
+    },
+    {
+        image:require("../assets/images/soft-drink.png"),
+        text:"Soft Drinks"
+    },
+    {
+        image:require("../assets/images/bread.png"),
+        text:"Bakery Items"
+    },
+    {
+        image:require("../assets/images/fast-food.png"),
+        text:"Fast-Foods"
+    },
+    {
+        image:require("../assets/images/deals.png"),
+        text:"Deals"
+    },
+    {
+        image:require("../assets/images/coffee.png"),
+        text:"Coffee & Tea"
+    },
+    {
+        image:require("../assets/images/desserts.png"),
+        text:"Desserts"
+    },
 
+]
 export default function Categories() {
     return (
-        <View>
-            <Text>category</Text>
+        <View
+        style={{
+            marginTop:5,
+            backgroundColor:"#fff",
+            paddingVertical:10,
+            paddingLeft:20
+        }}
+        >
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {
+                items.map((e,i)=>
+                     (
+
+            <View key={i} style={{alignItems:'center',marginRight:30}}>
+
+           <Image  source={e.image}
+           style={{
+               width:50,
+               height:40,
+               resizeMode:"contain"
+           }}
+           />
+            <Text style={{fontSize:13,fontWeight:"900"}}>{e.text}</Text>
+            </View>
+
+                    )
+                )
+            }
+
+        </ScrollView>
         </View>
     )
 }
