a08e5095999f553ddc26f13dee29dace0c27d52c
firas-debich <debichf02@gmail.com>
2021-12-30 12:50:07 +0100

    adding about component and restaurant detail

---
+++ front-side/Screens/RestaurantDetail.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Divider } from 'react-native-elements/dist/divider/Divider'
+import About from '../Components/RestourantDetail/About';
+
+export default function RestaurantDetail() {
+    return (
+        <View>
+            <About/>
+            <Divider width={1.8} style={{
+                marginVertical:20
+            }} />
+        </View>
+    )
+}
