cfa08c18dbb9a7d153b48afe0dc1109ed710930b
firas-debich <debichf02@gmail.com>
2021-12-31 20:05:55 +0100

    navigator added

---
+++ front-side/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from './Screens/Home';
+import RestaurantDetail from './Screens/RestaurantDetail';
+
+export default function Navigation() {
+  const Stack = createStackNavigator();
+
+  const screenOption = {
+    headerShown: false,
+  };
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOption}>
+          <Stack.Screen  name="Home" component={Home}/>
+          <Stack.Screen name="RestaurantDetail"  component={RestaurantDetail}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
