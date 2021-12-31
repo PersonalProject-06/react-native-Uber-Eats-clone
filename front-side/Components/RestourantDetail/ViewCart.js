278af785ad8ac71bc82b899125d8090441eff025
firas-debich <debichf02@gmail.com>
2021-12-31 23:55:02 +0100

    count the price done

--- front-side/Components/RestourantDetail/ViewCart.js
+++ front-side/Components/RestourantDetail/ViewCart.js
@@ -1,40 +1,46 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
-import { useSelector } from 'react-redux'
-
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useSelector } from "react-redux";
 
 export default function ViewCart() {
-    const items = useSelector((state)=>state.cartReducer.selectedItems.items)
-    const total = items .map((item)=>Number(item.price.replace("$",""))).reduce((prev,curr)=>prev+curr,0)
-    return (
-        <View style={{
-            flex:1,
-            alignItems:'center',
-            justifyContent:'center',
-            flexDirection:"row",
-            position:'absolute',
-            bottom:10,
-            zIndex:999
-        }}>
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((prev, curr) => prev + curr, 0);
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        position: "absolute",
+        bottom: 10,
+        zIndex: 999,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            marginTop: 20,
+            backgroundColor: "black",
+            alignItems: "center",
+            padding: 13,
+            borderRadius: 30,
+            width: 300,
+            position: "relative",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 20 }}>View Cart ${total}</Text>
 
-        <View style={{
-            flexDirection:"row",
-            justifyContent:'center',
-            width:"100%"
-        }}>
-            <TouchableOpacity style={{
-                marginTop:20,
-                backgroundColor:"black",
-                alignItems:"center",
-                padding:13,
-                borderRadius:30,
-                width:300,
-                position:'relative'
-            }}>
-            <Text style={{color:"white",fontSize:20}} >View Cart </Text>
-
-            </TouchableOpacity>
-        </View>
-                </View>
-    )
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
