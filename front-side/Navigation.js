a556e605e9421574ca579b7f35dc9d2b6cadc2c3
firas-debich <debichf02@gmail.com>
2021-12-31 22:27:59 +0100

    fixing the issues with redux

--- front-side/Navigation.js
+++ front-side/Navigation.js
@@ -3,7 +3,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from './Screens/Home';
 import RestaurantDetail from './Screens/RestaurantDetail';
-
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./Redux/Store";
+const store = configureStore()
 export default function Navigation() {
   const Stack = createStackNavigator();
 
@@ -11,11 +13,14 @@ export default function Navigation() {
     headerShown: false,
   };
   return (
+      <ReduxProvider store={store}>
+
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={screenOption}>
           <Stack.Screen  name="Home" component={Home}/>
           <Stack.Screen name="RestaurantDetail"  component={RestaurantDetail}/>
       </Stack.Navigator>
     </NavigationContainer>
+      </ReduxProvider>
   );
 }
--- front-side/Redux/Reducers/CartReducer.js
+++ front-side/Redux/Reducers/CartReducer.js
@@ -1,3 +1,23 @@
 let DefaultState = {
-    selectedItems : {}
-}
+  selectedItems: { items: [], restaurantName: "" },
+};
+
+let cartReducer = (state = DefaultState, action) => {
+  switch (action.type) {
+    case "ADD_TO_CARD": {
+      let newState = { ...state };
+      newState.selectedItems = {
+        items: [...newState.selectedItems.items, action.payload],
+        restaurantName: action.payload.restaurantName,
+      };
+      console.log(newState);
+      return newState
+    }
+    default :
+    return state
+  }
+
+};
+
+
+export default cartReducer
