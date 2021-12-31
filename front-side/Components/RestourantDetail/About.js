dc688248c0f69a5bb26ad946ad9667eeaba1d4f5
firas-debich <debichf02@gmail.com>
2021-12-31 20:29:58 +0100

    on press pass data works

--- front-side/Components/RestourantDetail/About.js
+++ front-side/Components/RestourantDetail/About.js
@@ -3,20 +3,13 @@ import { View, Text } from "react-native";
 import { Image } from "react-native";
 
 
-const Restaurant_Info ={
-    name:"test",
-    image:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7dOpgQrbqVA0Iyxnj-8_hDkTfFh0QauuqfA&usqp=CAU",
-    price:"",
-    reviews:"1500",
-    rating:4.5,
-    categories:[{title:"Thai"},{title:"Comfort Food"}]
-}
 
-const { name , image , price ,reviews ,rating ,categories}=Restaurant_Info
-const  FormatedCategories =categories.map((e)=>e.title).join(" • ")     
-const description = `${FormatedCategories} ${  price ? " • " + price : "" } • 🎫 • ${rating} ⭐ (${reviews}+)`;
-
-export default function About() {
+export default function About({route}) {
+    const {params}=route
+    const { name , image , price ,reviews ,rating ,categories}=params
+    const  FormatedCategories =categories.map((e)=>e.title).join(" • ")     
+    const description = `${FormatedCategories} ${  price ? " • " + price : "" } • 🎫 • ${rating} ⭐ (${reviews}+)`;
+   
   return (
     <View>
       <RestaurantImage imageUrl={image} />
