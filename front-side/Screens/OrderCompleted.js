1446bcbc9f9bc984f0b2a022a9ec7e3c06100c1d
firas-debich <debichf02@gmail.com>
2022-01-02 14:24:12 +0100

    installing lottie View react native

--- front-side/Screens/OrderCompleted.js
+++ front-side/Screens/OrderCompleted.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function OrderCompleted({route}) {
     console.log(route.params.total);
     return (
+        <SafeAreaView style={{
+            backgroundColor:"white",flex:1
+        }}>
+
         <View>
             <Text>Your order at {route.params.restaurantName} has been placed for {route.params.total} </Text>
         </View>
+        </SafeAreaView>
     )
 }
