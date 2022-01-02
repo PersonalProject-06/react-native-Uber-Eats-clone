520886ef06dbbfb3dd469fb52a447ff9c65d461b
firas-debich <debichf02@gmail.com>
2022-01-02 14:34:37 +0100

    lottie View animation worsk

--- front-side/Screens/OrderCompleted.js
+++ front-side/Screens/OrderCompleted.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import LottieView from "lottie-react-native";
 export default function OrderCompleted({route}) {
-    console.log(route.params.total);
+
     return (
         <SafeAreaView style={{
             backgroundColor:"white",flex:1
         }}>
-
+  <LottieView
+          style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
+          source={require("../assets/animations/check-mark.json")}
+          autoPlay
+          speed={0.5}
+          loop={false}
+        />
         <View>
             <Text>Your order at {route.params.restaurantName} has been placed for {route.params.total} </Text>
         </View>
