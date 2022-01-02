2d21e1d4319f67baf5bd81efc50eb6903f53dd50
firas-debich <debichf02@gmail.com>
2022-01-02 13:42:21 +0100

    adding dot env

--- front-side/firbase.js
+++ front-side/firbase.js
@@ -4,13 +4,13 @@ import {  APIKEY,AUTHDOMAIN ,PROJECTID ,STORAGEBUCKET ,MESSAGING_SENDER_ID,APP_I
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey:  "AIzaSyCer53q6k8KPU6F5LZ_MkTGOPS7yt4gbdE",
-    authDomain:  "uber-clone-336709.firebaseapp.com",
-    projectId: "uber-clone-336709",
-    storageBucket: "uber-clone-336709.appspot.com",
-    messagingSenderId: "1050065178129",
-    appId: "1:1050065178129:web:bd6b610744d61555feebb0",
-    measurementId: "G-DVXRLXMZJ5"
+    apiKey:  APIKEY,
+    authDomain:  AUTHDOMAIN,
+    projectId: PROJECTID,
+    storageBucket: STORAGEBUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
   };
 
 
